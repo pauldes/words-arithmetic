b1d556bf355a72732418d16dd210cbcee71ef5dd
pauldes <pauldesigaud@outlook.fr>
2021-05-30 00:34:48 +0200

    Change packaging

--- words_arithmetic/__init__.py
+++ words_arithmetic/__init__.py
@@ -1,23 +1,20 @@
 import importlib
 import pkgutil
+import sys
 
 
-def import_submodules(package, recursive=True):
-    """ Import all submodules of a module, recursively, including subpackages
+def import_submodules(package_name):
+    """ Import all submodules of a module, recursively
 
-    :param package: package (name or actual module)
-    :type package: str | module
-    :rtype: dict[str, types.ModuleType]
+    :param package_name: Package name
+    :type package_name: str
+    :rtype: dict[types.ModuleType]
     """
-    if isinstance(package, str):
-        package = importlib.import_module(package)
-    results = {}
-    for loader, name, is_pkg in pkgutil.walk_packages(package.__path__):
-        full_name = package.__name__ + '.' + name
-        results[full_name] = importlib.import_module(full_name)
-        if recursive and is_pkg:
-            results.update(import_submodules(full_name))
-    return results
+    package = sys.modules[package_name]
+    return {
+        name: importlib.import_module(package_name + '.' + name)
+        for loader, name, is_pkg in pkgutil.walk_packages(package.__path__)
+    }
 
 
-import_submodules(__name__)
+__all__ = import_submodules(__name__).keys()
