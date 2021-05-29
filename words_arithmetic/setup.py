97be172d37d4adfeedd16038c48e2986d1e3419b
pauldes <pauldesigaud@outlook.fr>
2021-05-29 12:36:54 +0200

    Install package

---
+++ words_arithmetic/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="words-arithmetic",
+    version="0.0.1",
+    description="Package used for the application words_arithmetic",
+    packages=[],
+    python_requires="==3.8.7",
+)
